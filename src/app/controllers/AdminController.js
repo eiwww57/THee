dca27fce04cc315361b6766e2f887f1fb129fd60
Cashion <cashion1406@gmail.com>
2021-11-02 00:15:38 +0700

    Fix Update nfo

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -49,7 +49,7 @@ class AdminController {
 
     updateProduct(req,res){
         
-        req.body.imglink = "imgs/" + req.body.imglink
+      
         Product.updateOne({_id:req.params.id}, req.body)
         .then(data=>{
             res.redirect('/admin/viewproducts');
